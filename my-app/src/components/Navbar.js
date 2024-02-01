7540cb923bbf063f567cf5fb72abd49fdedf1a50
iTakedown27 <ezrabridger27@gmail.com>
2024-01-31 23:55:04 -0800

    setup react

---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <nav>
+        <ul>
+          <li><a>Home</a></li>
+          <li><a>View Map</a></li>
+          <li><a>Add Utility Info</a></li>
+          <li><a>Login</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
