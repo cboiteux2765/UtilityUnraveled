811124dcceedbe46d0d770c37760188a5fc6d2b5
itswill307 <102396164+itswill307@users.noreply.github.com>
2024-05-07 22:26:17 -0700

    added nativewind and ios simulator ability

--- README.md
+++
@@ -1,2 +0,0 @@
-# UtilityUnraveled
-A website that helps UCSB students find the quality of certain utilities on campus, from a laundry room to water fountain to a dining hall.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./App.{js,jsx,ts,tsx}"], // add "./<custom directory>/**/*.{js,jsx,ts,tsx}"
   theme: {
     extend: {},
   },
